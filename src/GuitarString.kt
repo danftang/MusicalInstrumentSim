fb10cc90dc6147f17a56995b96357defa679aca9
Daniel Tang <from_github@freshjava.co.uk>
2018-07-12 21:25:10 +0100

    added energy loss to bridge and neck

--- src/GuitarString.kt
+++ src/GuitarString.kt
@@ -1,11 +1,13 @@
 class GuitarString {
-    val N = 100             // number of masses
+    val N = 50             // number of masses
     val dx = 1.0 / 200.0      // distance between masses (m)
-    val rho = 0.2           // weight of string per metre (kg/m)
+    val rho = 0.1           // weight of string per metre (kg/m)
     val m = rho * dx          // weight of each mass (kg)
-    val T = 15              // tension in the string (N)
-    val dt = 0.00000005           // timestep (s)
-    val f = 0.002           // energy loss due to air friction (Ns/m)
+    val T = 50              // tension in the string (N)
+    val dt = 0.00000002           // timestep (s)
+    val f = 0.0005           // energy loss due to air friction (Ns/m)
+    val fBridge= 0.1       // energy loss to the bridge (Ns/dx)
+    val fNeck = 0.05         // energy loss to the neck (Ns/dx)
 
     val x = DoubleArray(N, { 0.0 }) // string position, initialised at equilibrium
     val v = DoubleArray(N, { 0.0 }) // velocity of string, initially at rest
@@ -14,13 +16,14 @@ class GuitarString {
         var t = 0.0
         while(t<DT) {
             step()
-            t += dt;
+            t += dt
         }
     }
 
     fun step() {
         val a = DoubleArray(N, {i ->
-            T*d2_dx2(x,i)/(m*dx) - f*v[i]/m
+            val friction = if(i<2) fBridge else if(i > N-3) fNeck else f
+            T*d2_dx2(x,i)/(m*dx) - friction*v[i]/m
         }) // acceleration of the string
         for(i in 0 until N) {
             x[i] += v[i]*dt + 0.5*a[i]*dt*dt
--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,12 @@
+import kotlin.math.sin
 
 fun main(args : Array<String>) {
     val string = GuitarString()
     val DT = 1.0/44100.0
-    val steps = (5.0/DT).toInt()
+    val steps = (6.0/DT).toInt()
     val audioData = DoubleArray(steps)
 
-    string.hit(80) // hit the string
+    string.hit(25) // hit the string
     for(t in 0 until steps) {
         string.step(DT)
         audioData[t] = string.forceOnBridge()
